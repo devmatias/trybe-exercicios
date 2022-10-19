7be9b4736a8d942601aaa9ab299e2677ee249e82
devmatias <devmatiascff@gmail.com>
2022-10-19 12:35:22 -0300

    Exercicio fixacao variaveis

---
+++ fundamentos/secao-04-introducao-javascript/dia-01-js-primeiros-passos/exercicio1/script.js
@@ -0,0 +1,8 @@
+const myName = 'Matias';
+const birthCity = 'Berna';
+let birthYear = 1993;
+console.log("Meu nome é " + myName + ", nasci na cidade de " + birthCity + " e nasci no ano de " + birthYear)
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "São Paulo"
+console.log(birthCity)
