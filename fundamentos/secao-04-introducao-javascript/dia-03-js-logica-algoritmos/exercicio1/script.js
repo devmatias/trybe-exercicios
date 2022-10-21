2d381dd7281995e908c7538e47e62d53ae79e4cb
devmatias <devmatiascff@gmail.com>
2022-10-21 18:58:28 -0300

    Finalizado exemplos de fixação

---
+++ fundamentos/secao-04-introducao-javascript/dia-03-js-logica-algoritmos/exercicio1/script.js
@@ -0,0 +1,17 @@
+// const n = 9 ;
+
+// let resultado = 100;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado -= index;
+//   console.log(resultado);
+// };
+// console.log(resultado);
+
+const n = 100;
+
+let resultado = 0;
+for (let index = 0; index <= n; index += 1) {
+  resultado += index;
+  console.log(resultado)
+};
+console.log(resultado)
