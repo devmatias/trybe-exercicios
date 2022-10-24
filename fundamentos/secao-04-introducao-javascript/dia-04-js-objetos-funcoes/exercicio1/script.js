32caaa648b081e83f57b4dce8c761a9820ba9640
devmatias <devmatiascff@gmail.com>
2022-10-24 18:46:26 -0300

    Exercicio Fixacao parte 2

--- fundamentos/secao-04-introducao-javascript/dia-04-js-objetos-funcoes/exercicio1/script.js
+++ fundamentos/secao-04-introducao-javascript/dia-04-js-objetos-funcoes/exercicio1/script.js
@@ -1,16 +1,40 @@
-let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 };
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
 
-let player = {
-    name :'Marta',
-    lastName : 'Silva',
-    age : 34,
-    medals : {
-        golden : 2,
-        silver : 3,
-    }
-}
+// let player = {
+//     name :'Marta',
+//     lastName : 'Silva',
+//     age : 34,
+//     medals : {
+//         golden : 2,
+//         silver : 3,
+//     }
+// }
 
-console.table(player);
+// console.table(player);
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+// for(index in names){
+//     console.log("Olá", names[index]);
+// }
+
+// let namesEntries = Object.entries(names);
+// console.log(namesEntries);
+
+// for(names of namesEntries){
+//     console.log(names)
+// }
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+
