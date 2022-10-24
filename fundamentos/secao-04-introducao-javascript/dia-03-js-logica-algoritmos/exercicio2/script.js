4b5bf6fd36c98064cf69da17771e0417b2f3da5f
devmatias <devmatiascff@gmail.com>
2022-10-24 12:23:25 -0300

    Finalizado exercicio2

--- fundamentos/secao-04-introducao-javascript/dia-03-js-logica-algoritmos/exercicio2/script.js
+++ fundamentos/secao-04-introducao-javascript/dia-03-js-logica-algoritmos/exercicio2/script.js
@@ -7,6 +7,16 @@
 // }
 
 // let word = 'tryber';
+// let reverseWord = '';
+
+// for (let index = 0; index < word.length; index += 1) {
+//   reverseWord += word[word.length - 1 - index];
+//   console.log(reverseWord);
+// }
+
+// console.log(reverseWord);
+
+// let word = 'tryber';
 // let wordArray = word.split("");
 // console.log(wordArray.length - 1);
 // let wordReverseArray = [];
@@ -19,14 +29,41 @@
 // let wordReverse = wordReverseArray.join('');
 // console.log(wordReverse);
 
-let array = [ 'java', 'python', 'html', 'css', 'javascript'];
-let maior = "";
+// let array = [ 'java', 'python', 'html', 'css', 'javascript'];
+// let maior = "";
+// let menor = null;
 
-for(let index = 0; index < array.length; index++){
-    console.log(array[index])
-    if(array[index].length > maior.length) {
-        maior = array[index];
+// for(let index = 0; index < array.length; index++){
+//     console.log(array[index])
+//     if(array[index].length > maior.length) {
+//         maior = array[index];
+//     }
+// }
+
+// for(let index = 0; index < array.length; index++){
+//     if(menor == null){
+//         menor = array[index];
+//         console.log(menor);
+//     } else if(menor > array[index]) {
+//         menor = array[index];
+//         console.log(menor);
+//     }
+// }
+
+// console.log(maior);
+// console.log(menor);
+
+let primos = [];
+
+for(let index = 2; index <= 50; index++){
+    let cont = 0;
+    for(let i = 1; i <= index; i++){
+        if(index % i === 0) {
+            cont++;
+        }
+        if(index === i && cont === 2){
+            primos.push(index);
+        }
     }
+    console.log(primos)
 }
-
-console.log(maior);
