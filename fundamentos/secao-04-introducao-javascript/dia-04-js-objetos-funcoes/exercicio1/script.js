912abedd31b5451ab06555ef92f1ffd10b1ab9a4
devmatias <devmatiascff@gmail.com>
2022-10-24 19:42:52 -0300

    Exercicio Fixacao parte 3

--- fundamentos/secao-04-introducao-javascript/dia-04-js-objetos-funcoes/exercicio1/script.js
+++ fundamentos/secao-04-introducao-javascript/dia-04-js-objetos-funcoes/exercicio1/script.js
@@ -38,3 +38,95 @@
 //     year: 2020
 //   };
 
+function soma(a, b) {
+    return a + b;
+}
+
+function subtracao(a, b) {
+    return a - b;
+}
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    return a / b;
+}
+
+function modulo(a, b) {
+    return a % b;
+}
+
+console.log(soma(10, 10));
+console.log(subtracao(10, 10));
+console.log(multiplicacao(10, 10));
+console.log(divisao(10, 10));
+console.log(modulo(10, 3));
+
+function maior(a, b) {
+    let resultado;
+    if(a > b) {
+        resultado = a;
+    } else if(b > a) {
+        resultado = b;
+    } else { 
+        resultado = "Os números são iguais";
+    }
+    return resultado;
+}
+
+console.log(maior(20, 20));
+
+function maiorEm3(a, b, c) {
+    let resultado;
+    if(a > b && a > c) {
+        resultado = a;
+    } else if(b > c && b > a) {
+        resultado = b;
+    } else if(c > a && c > b){ 
+        resultado = c;
+    } else if(a === b && a === c){
+        resultado = "Os números são iguais!";
+    } else if(a === b && a > c) {
+        resultado = "Os números em a e b são iguais e maiores que c!";
+    } else if(b === c && b > a) {
+        resultado = "Os números em b e c são iguais e maiores que a!";
+    } else if(a === c && a > b) {
+        resultado = "Os números em a e c são iguais e maiores que b!";
+    }
+    return resultado;
+}
+
+console.log(maiorEm3(50, 60, 80));
+
+function descobrirSinal(a) {
+    let resultado;
+    if(a > 0) {
+        resultado = "positivo";
+    } else if(a < 0){
+        resultado = "negativo";
+    } else {
+        resultado = "zero";
+    }
+    return resultado;
+}
+
+
+console.log(descobrirSinal(0));
+
+function descobrirPoligono(a, b, c){
+    let resultado;
+    if(a > 0 && b > 0 && c > 0) {
+        if(a + b + c === 180) {
+            resultado = "O polígono é um triângulo!"
+        } else {
+            resultado = "O polígono não é um triângulo!"
+        }
+    } else {
+        resultado = "Parâmetros inválidos!"
+    }
+    return resultado;
+}
+
+console.log(descobrirPoligono(25, 140, 15))
