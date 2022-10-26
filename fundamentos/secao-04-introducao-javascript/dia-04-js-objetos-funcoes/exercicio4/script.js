58c20738322b185ca7428bf0a3572c782e190da5
devmatias <devmatiascff@gmail.com>
2022-10-26 10:02:42 -0300

    Exercicio sobre iteração em objetos

--- fundamentos/secao-04-introducao-javascript/dia-04-js-objetos-funcoes/exercicio4/script.js
+++ fundamentos/secao-04-introducao-javascript/dia-04-js-objetos-funcoes/exercicio4/script.js
@@ -36,7 +36,7 @@
 //                     console.log("resultado atual:", resultado);
 //                     console.log("holder atual:", holder);
 //                 }
-                
+
 //                 break;
 //             }
 //         }
@@ -64,43 +64,87 @@
 
 // console.log(arrayOfNumbers(vector));
 
-const basket = [
-    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
-    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
-    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
-    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
-    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
-    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
-    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
-    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
-    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
-    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
-    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
-    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
-    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
-    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
-    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
-    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
-    'Banana', 'Pera', 'Abacate', 'Uva',
-  ];
-  
-  const result = {};
-  
-  for (let index = 0; index < basket.length; index += 1) {
-    const fruit = basket[index];
-    if (!result[fruit]) result[fruit] = 0;
-    result[fruit] += 1;
-    // console.log(result)
+// const basket = [
+//     'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+//     'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+//     'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+//     'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+//     'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+//     'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+//     'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+//     'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+//     'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+//     'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+//     'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+//     'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+//     'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+//     'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+//     'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+//     'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+//     'Banana', 'Pera', 'Abacate', 'Uva',
+//   ];
+
+//   const result = {};
+
+//   for (let index = 0; index < basket.length; index += 1) {
+//     const fruit = basket[index];
+//     if (!result[fruit]) result[fruit] = 0;
+//     result[fruit] += 1;
+//     // console.log(result)
+//   }
+
+//   const summaries = [];
+
+//   for (fruit in result) {
+//     let message = result[fruit] + " " + fruit;
+//     console.log(message);
+//     if (result[fruit] > 1) message += 's';
+//     summaries.push(message);
+//   }
+
+//   console.log('Sua cesta possui: ' + summaries.join(', '));
+//   console.log(result)
+
+let moradores = {
+  blocoUm: [
+    {
+      nome: "Luiza",
+      sobrenome: "Guimarães",
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: "William",
+      sobrenome: "Albuquerque",
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [
+    {
+      nome: "Murilo",
+      sobrenome: "Ferraz",
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: "Zoey",
+      sobrenome: "Brooks",
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
+
+// console.log("O morador do bloco 2 de nome " +
+//  moradores.blocoDois[1].nome, moradores.blocoDois[1]['sobrenome'] + " mora no " +
+//   moradores.blocoDois[1]['andar'] + "° andar, apartamento " + moradores.blocoDois[1]['apartamento']);
+
+for(bloco in moradores) {
+  for(let index = 0; index < moradores[bloco].length; index++){
+    console.log("É um morador do " + bloco + " o(a) morador(a) " + moradores[bloco][index].nome, moradores[bloco][index].sobrenome);
+    
   }
-  
-  const summaries = [];
-  
-  for (fruit in result) {
-    let message = result[fruit] + " " + fruit;
-    console.log(message);
-    if (result[fruit] > 1) message += 's';
-    summaries.push(message);
-  }
-  
-  console.log('Sua cesta possui: ' + summaries.join(', '));
-  console.log(result)
+  // console.log(moradores[bloco][0].nome, moradores[bloco][0].sobrenome)
+  // console.log(moradores[bloco][1].nome, moradores[bloco][1].sobrenome)
+}
