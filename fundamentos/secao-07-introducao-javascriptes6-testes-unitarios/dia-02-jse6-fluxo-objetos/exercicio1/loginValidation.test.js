9b4112d55c32c00f297c8e35b51722c05adda2e5
devmatias <devmatiascff@gmail.com>
2022-11-20 12:23:10 -0300

    Exemplos de test1

--- fundamentos/secao-07-introducao-javascriptes6-testes-unitarios/dia-02-jse6-fluxo-objetos/exercicio1/loginValidation.test.js
+++ fundamentos/secao-07-introducao-javascriptes6-testes-unitarios/dia-02-jse6-fluxo-objetos/exercicio1/loginValidation.test.js
@@ -27,9 +27,9 @@ const {
       );
     });
   
-    it('loginErrorMessage() returns a message in the format: `Pessoa usuária '${user}' não encontrada, tente novamente!`', () => {
-        expect(loginErrorMessage('Bob')).toBe(
-          'Pessoa usuária 'Bob' não encontrada, tente novamente!'
-        );
-    });
+    // it('loginErrorMessage() returns a message in the format: `Pessoa usuária '${user}' não encontrada, tente novamente!`', () => {
+    //     expect(loginErrorMessage('Bob')).toBe(
+    //       'Pessoa usuária 'Bob' não encontrada, tente novamente!'
+    //     );
+    // });
   });
