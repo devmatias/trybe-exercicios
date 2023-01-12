9fd1a0f7614e1c678b6e3c87c5c8368e36e5f377
devmatias <devmatiascff@gmail.com>
2023-01-12 18:03:34 -0300

    Exercicio pratico casa de cambio

---
+++ front-end/secao-01-javascript-testes-assincronos/pratica-casa-cambio/src/main.js
@@ -0,0 +1,87 @@
+import "./style.css";
+import Swal from 'sweetalert2';
+
+const customSwal = Swal.mixin({
+  customClass: {
+    title: 'swal-title',
+    text: 'btn btn-danger'
+  },
+})
+
+function createCurrencyBlock(fatherElement) {
+  const newRate = document.createElement('div');
+  newRate.classList.add('currency-block');
+  const newFirstElementBlock = document.createElement('div');
+  newFirstElementBlock.classList.add('first-element');
+  const newCoinImage = document.createElement('img');
+  newCoinImage.src = "./src/imgs/coins.png";
+  newCoinImage.alt = 'coins';
+  const newHeading5 = document.createElement('h5');
+  newFirstElementBlock.appendChild(newCoinImage);
+  newFirstElementBlock.appendChild(newHeading5);
+  newRate.appendChild(newFirstElementBlock);
+  const newHeading6 = document.createElement('h6');
+  newRate.appendChild(newHeading6);
+  fatherElement.appendChild(newRate);
+}
+
+function deleteChild() {
+  const currencyContainer = document.querySelector(".currency-container");
+  var child = currencyContainer.lastElementChild; 
+  while (child) {
+      currencyContainer.removeChild(child);
+      child = currencyContainer.lastElementChild;
+  }
+}
+
+function searchCurrency() {
+  const currencyContainer = document.querySelector('.currency-container')
+  const inputCurrency = document.querySelector('.input-currency');
+  const searchButton = document.querySelector('.btn-search')
+  const url = 'https://api.exchangerate.host/latest?base=';
+
+  searchButton.addEventListener('click', () => {
+    if(!inputCurrency.value) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Ops...',
+        text: 'Você precisa passar uma moeda!',
+        background: '#303030',
+        color: '#FFF',
+      })
+      return;
+    }
+
+    deleteChild()
+
+    fetch(url + inputCurrency.value)
+      .then(response => response.json())
+      .then(data => {
+        const { rates } = data;
+        if(!Object.keys(rates).some((rate) => rate === inputCurrency.value)) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Ops...',
+            text: 'Moeda não existente!',
+            background: '#303030',
+            color: '#FFF',
+          })
+          return;
+        }
+        const headingContainer = document.createElement('h2');
+        headingContainer.innerHTML = `Valores referentes a 1 ${inputCurrency.value}`;
+        currencyContainer.appendChild(headingContainer)
+        console.log(rates)
+        for(let rate in rates) {
+          createCurrencyBlock(currencyContainer)
+          const getCurrencyBlocks = document.querySelectorAll('.currency-block');
+          getCurrencyBlocks[getCurrencyBlocks.length - 1].firstElementChild.lastElementChild.innerHTML = rate;
+          getCurrencyBlocks[getCurrencyBlocks.length - 1].lastElementChild.innerHTML = Number(rates[rate]).toFixed(2);
+        }
+      })
+
+  })
+}
+
+searchCurrency()
+
