801184f6b2284d27f8d1e55dda5238e43aff5c28
devmatias <devmatiascff@gmail.com>
2023-01-20 12:30:33 -0300

    Começando secao 2 com react

---
+++ front-end/secao-02-introducao-ao-react/hello-world-react/parte2/src/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (<>
+      <h1> Matias Correa</h1 >
+      <p>Sou uma pessoa muito legal</p>
+      <h2>Minhas habilidades</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>React</li>
+      </ul>
+    </>
+    )
+  }
+};
+
+export default About;
