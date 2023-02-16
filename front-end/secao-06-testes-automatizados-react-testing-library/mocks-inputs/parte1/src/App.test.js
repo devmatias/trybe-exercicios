3c36a3f3104d1b8db061310f9e9c0b0e6cdfc543
devmatias <devmatiascff@gmail.com>
2023-02-16 18:39:19 -0300

    exercicio fixacao dia de mocks

---
+++ front-end/secao-06-testes-automatizados-react-testing-library/mocks-inputs/parte1/src/App.test.js
@@ -0,0 +1,53 @@
+// App.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+
+afterEach(() => jest.clearAllMocks());
+
+it('fetches a joke', async () => {
+  const joke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke),
+  });
+
+  render(<App />);
+  const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
+  expect(renderedJoke).toBeInTheDocument();
+  expect(global.fetch).toHaveBeenCalledTimes(1);
+  expect(global.fetch).toHaveBeenCalledWith(
+    'https://icanhazdadjoke.com/',
+    { headers: { Accept: 'application/json' } },
+  );
+});
+
+it('test button fetch', async () => {
+  const joke = {
+    id: '7h3oGtrOfx3',
+    joke: 'Nova Piada',
+    status: 200,
+  };
+
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke),
+  });
+
+  render(<App />);
+  const newJokeBtn = screen.getByRole('button')
+  userEvent.click(newJokeBtn)
+  const renderedJoke = await screen.findByText('Nova Piada');
+  expect(renderedJoke).toBeInTheDocument();
+  expect(global.fetch).toHaveBeenCalledTimes(2);
+  expect(global.fetch).toHaveBeenCalledWith(
+    'https://icanhazdadjoke.com/',
+    { headers: { Accept: 'application/json' } },
+  );
+});
